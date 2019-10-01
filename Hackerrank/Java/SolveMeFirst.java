8eb5996e68eed19dca0a2a9d79375aacaf579e8d
coderkotlin <coderkotlin@gmail.com>
2019-10-01 12:14:30 +0700

    Create example problem solving in Java language

---
+++ Hackerrank/Java/SolveMeFirst.java
@@ -0,0 +1,20 @@
+/**
+ * https://hackerrank.com/challenges/solve-me-first/problem
+ */
+
+ public class SolveMeFirst {
+     static int solveMeFirst(int a, int b) {
+         return a+b;
+     }
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+         int a;
+         a = in.nextInt();
+         int b;
+         b = in.nextInt();
+         int sum;
+         sum = solveMeFirst(a, b);
+         System.out.println(sum);
+     }
+ }
