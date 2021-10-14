ee0f656230c8e4597a35ac657f2d4c13ea3b42e6
Renita Kurian <bluecoder1206@gmail.com>
2021-10-14 23:44:55 +0530

    Added solution to chocolate feast problem

---
+++ Hackerrank/Java/ChocolateFeast.java
@@ -0,0 +1,56 @@
+//https://www.hackerrank.com/challenges/chocolate-feast/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the chocolateFeast function below.
+    static int chocolateFeast(int n, int c, int m) 
+    {
+        int number=0,wrapper=0;
+        number+=n/c;
+        wrapper=number;
+        while(wrapper>=m )
+        {
+            int a=(wrapper/m);
+            wrapper=(wrapper%m)+a;
+            number+=a;
+        }
+        return number;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] ncm = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(ncm[0]);
+
+            int c = Integer.parseInt(ncm[1]);
+
+            int m = Integer.parseInt(ncm[2]);
+
+            int result = chocolateFeast(n, c, m);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
